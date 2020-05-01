63bf5bae03a689b25bdd36f7d76624fbf88bb976
oskiman18 <oscar__180894@hotmail.com>
2020-05-01 20:14:52 -0300

    anda?
    agregue Rutil

--- protos.h
+++ protos.h
@@ -12,9 +12,9 @@ while (continuar)
 cin>>opcion;
 switch(opcion)
  {  case 1: nuevo_plato();break;
-    case 2: break;
+   // case 2: break;
     case 3: punto_3();break;
-    case 4: break;
+   // case 4: break;
     case 5: listar_todo();break;
     case 6: break;
     case 0: continuar=false; break;
